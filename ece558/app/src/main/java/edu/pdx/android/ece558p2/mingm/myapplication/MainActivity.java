5b59b25f78dd67c53f062ffe5c164d0bcfe89bb7
Ming Ma <mming@pdx.edu>
2018-11-08 23:19:15 -0800

    Update project

---
+++ ece558/app/src/main/java/edu/pdx/android/ece558p2/mingm/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.pdx.android.ece558p2.mingm.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //This is the test
+    }
+}
